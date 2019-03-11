3023feec6904cd84ef53e45e51fd930b18622837
Jan Tattermusch <jtattermusch@google.com>
2019-03-11 03:28:10 -0400

    try fetching dotnet SDK before build

---
+++ csharp/get-dotnet.ps1
@@ -0,0 +1,26 @@
+#!/usr/bin/env powershell
+
+Set-StrictMode -Version 2
+$ErrorActionPreference = 'Stop'
+
+$WorkingDir = $PSScriptRoot
+$TempDir = Join-Path $WorkingDir 'obj'
+$InstallScriptUrl = 'https://dot.net/v1/dotnet-install.ps1'
+$InstallScriptPath = Join-Path $TempDir 'dotnet-install.ps1'
+$GlobalJsonPath = Join-Path $WorkingDir '..' | Join-Path -ChildPath 'global.json'
+
+function Ensure-Dir([string]$path) {
+    if (!(Test-Path $path -PathType Container)) {
+        New-Item -ItemType Directory -Force -Path $path | Out-Null
+    }
+}
+
+# Resolve SDK version
+$GlobalJson = Get-Content -Raw $GlobalJsonPath | ConvertFrom-Json
+$SDKVersion = $GlobalJson.sdk.version
+
+# Download install script
+Ensure-Dir $TempDir
+Write-Host "Downloading install script: $InstallScriptUrl => $InstallScriptPath"
+Invoke-WebRequest -Uri $InstallScriptUrl -OutFile $InstallScriptPath
+&$InstallScriptPath -Version $SDKVersion
--- kokoro/release/csharp/windows/build_nuget.bat
+++ kokoro/release/csharp/windows/build_nuget.bat
@@ -2,4 +2,7 @@
 cd /d %~dp0\..\..\..\..
 
 cd csharp
+
+powershell -File get-dotnet.ps1
+
 call build_packages.bat
